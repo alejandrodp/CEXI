6c083abc48eef8d4d7e0a49f943c861f03440a79
machetazo <fabianmontero1998@gmail.com>
2018-03-21 21:55:29 -0600

    [MERGE] Pre-merge commit

--- src/launcher.cpp
+++ src/launcher.cpp
@@ -1,11 +1,17 @@
-#include "compiler.h"
-
+//#include "compiler.h"
+# include <iostream>
 // TODO: arreglar el include
 int main() {
 
+/*
     auto *file = compiler::preprocessor("/home/fabian/Documents/repos/datos2/CEX/examples/helloworld.cex")
 
     cout << file << endl;
+*/
+
+    int *values[5];
+
+    std::cout << values << std::endl;
 
     return 0;
 }
